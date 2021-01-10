676bc8c3e4bcf654eb90a7e5678dcde9b29c9e2e
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-10 19:27:34 +0530

    Initial Commit

---
+++ Messaging App/Messaging_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Messaging_AppApp.swift
+//  Messaging App
+//
+//  Created by Mahesh Prasad on 10/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Messaging_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
